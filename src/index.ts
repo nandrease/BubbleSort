4be1ef1c34acdd1b272cbaeb576fc92336795ccd
nandrease <neeme.andreas.eller@gmail.com>
2020-05-16 15:17:38 +0300

    Move from interfaces to abstract classes

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,16 @@
 import { LinkedList } from './LinkedList';
 import { NumbersCollection } from './NumbersCollection';
-import { Sorter } from './Sorter';
 import { CharactersCollection } from './CharactersCollection';
 
-// const numbersCollection = new NumbersCollection([ 10, 3, -5, 0 ]);
-// console.log('Initial', numbersCollection.data);
-// const sorter = new Sorter(numbersCollection);
-// sorter.sort();
-// console.log('Sorted', numbersCollection.data);
+const numbersCollection = new NumbersCollection([ 10, 3, -5, 0 ]);
+console.log('Initial', numbersCollection.data);
+numbersCollection.sort();
+console.log('Sorted', numbersCollection.data);
 
-// const charactersCollection = new CharactersCollection('Suzuki GSX-R 750');
-// console.log('Unsorted', charactersCollection.data);
-// const sorter = new Sorter(charactersCollection);
-// sorter.sort();
-// console.log('Sorted', charactersCollection.data);
+const charactersCollection = new CharactersCollection('Suzuki GSX-R 750');
+console.log('Unsorted', charactersCollection.data);
+charactersCollection.sort();
+console.log('Sorted', charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(500);
@@ -21,6 +18,8 @@ linkedList.add(-10);
 linkedList.add(-3);
 linkedList.add(10);
 linkedList.add(99);
-const sorter = new Sorter(linkedList);
-sorter.sort();
+console.log('Before');
+linkedList.print();
+linkedList.sort();
+console.log('After');
 linkedList.print();
