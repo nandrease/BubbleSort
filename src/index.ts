7de4095a52ba522340a8a29bbe3085a7a768b176
nandrease <neeme.andreas.eller@gmail.com>
2020-05-16 14:05:57 +0300

    Sorting strings

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([ 10, 3, -5, 0 ]);
-console.log('Initial', numbersCollection.data);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([ 10, 3, -5, 0 ]);
+// console.log('Initial', numbersCollection.data);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log('Sorted', numbersCollection.data);
 
+const charactersCollection = new CharactersCollection('Suzuki GSX-R 750');
+console.log('Unsorted', charactersCollection.data);
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
-console.log('Sorted', numbersCollection.data);
+console.log('Sorted', charactersCollection.data);
