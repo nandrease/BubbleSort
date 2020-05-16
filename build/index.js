84045359f3b96885adc72a2f6d1dcbdf67ef3abf
nandrease <neeme.andreas.eller@gmail.com>
2020-05-16 14:04:45 +0300

    Remove build folder

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var NumbersCollection_1 = require("./NumbersCollection");
-var Sorter_1 = require("./Sorter");
-var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
-console.log('Initial', numbersCollection.data);
-var sorter = new Sorter_1.Sorter(numbersCollection);
-sorter.sort();
-console.log('Sorted', numbersCollection.data);
